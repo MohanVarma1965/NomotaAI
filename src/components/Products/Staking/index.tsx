f9ba504d79ed3887158641e5dc5473ce9d6e8529
casperdeleuw <casperdeleuw@gmail.com>
2024-03-30 15:18:17 +0100

    Added product page with sub page

---
+++ src/components/Products/Staking/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Staking: React.FC = () => {
+  return (
+    <div className="py-12 text-center bg-white max-md:p-8">
+      Staking
+    </div>
+  );
+};
+
+export default Staking;
---
+++ src/components/Products/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FeaturesSection from "../FeaturesSection";
+
+const Products: React.FC = () => {
+  return (
+    <div>
+      <FeaturesSection />
+    </div>
+  );
+};
+
+export default Products;
