8cda462e1bebfd68341f38803978f0c0d691fcc2
jastisudheer <sudheerjasti2002@gmail.com>
2024-03-31 01:39:28 +0530

    Added KeyFeatures Component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import MainPageBanner from "./MainPageBanner";
 import OnePlaceTiles from "./OnePlaceTiles";
+import KeyFeatures from "./KeyFeatures";
 import LearnMoreSection from "./LearnMoreSection";
 import Roadmap from "./Roadmap";
 import Tokenomics from "./Tokenomics";
 import FeaturesSection from "../FeaturesSection";
 import SocialIcons from "./SocialIcons";
 import { useTitle } from "../Effects";
-
 const Main: React.FC = () => {
   useTitle("Nomota AI - Where technology meets creativity")
   return (
     <div>
       <MainPageBanner />
       {/* <OnePlaceTiles /> */}
+      <KeyFeatures/>
       {/* <LearnMoreSection /> */}
       <FeaturesSection />
       <Roadmap />
