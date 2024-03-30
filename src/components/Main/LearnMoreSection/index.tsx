f3d33b0d6fb87701347400fa83a9f70a9c2cb4b8
bonamsrikanth1 <bonamsrikanthcan@gmail.com>
2024-03-30 16:41:04 +0530

    adjust padding on learnmore

--- src/components/Main/LearnMoreSection/index.tsx
+++ src/components/Main/LearnMoreSection/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const LearnMoreSection: React.FC = () => {
   return (
-    <div className="bg-white text-center py-12">
-      <h2 className="text-2xl md:text-4xl font-semibold mb-4">Learn more about Nokomota AI technology</h2>
-      <button className="bg-black text-white font-medium py-3 px-8 rounded-lg hover:bg-gray-800 transition-colors duration-200">
+    <div className="py-12 text-center bg-white max-md:p-8">
+      <h2 className="mb-4 text-2xl font-semibold md:text-4xl">Learn more about Nokomota AI technology</h2>
+      <button className="px-8 py-3 font-medium text-white transition-colors duration-200 bg-black rounded-lg hover:bg-gray-800">
         Discover Nokomota AI
       </button>
     </div>
