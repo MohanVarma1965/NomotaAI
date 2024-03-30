2e89ef7ceba205650173a6c95631ca7f26ee6914
bonamsrikanth1 <bonamsrikanthcan@gmail.com>
2024-03-30 08:58:08 +0530

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   variants: {
