1f0ad5ef14fb620eabce23dadb9d306c29ac393d
jastisudheer <sudheerjasti2002@gmail.com>
2024-03-30 12:10:37 +0530

    Updated Tokenomics

--- src/components/Main/Tokenomics/index.tsx
+++ src/components/Main/Tokenomics/index.tsx
@@ -1,69 +1,90 @@
-import React from "react";
-import { CheckCircleIcon, CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import React from 'react';
+import { CheckCircleIcon, CurrencyDollarIcon } from '@heroicons/react/24/outline';
 
 const Tokenomics: React.FC = () => {
   return (
     <div className="bg-gradient-to-b from-gray-800 to-gray-900 text-white py-12 px-4">
-      <div className="text-center mb-8">
+      <div className="text-center mb-12">
         <h2 className="text-4xl font-bold">Tokenomics</h2>
       </div>
-      <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
-        <div className="flex flex-col items-center">
-          <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center">
-            <span className="text-2xl">75%</span>
+      <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6">
+        {/* Tile 1 */}
+        <div className="bg-gray-700 rounded-xl p-6 shadow-lg transition duration-300 ease-in-out hover:scale-105">
+          <div className="flex justify-center">
+            <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center mb-4">
+              <span className="text-3xl font-bold">76%</span>
+            </div>
           </div>
-          <h3 className="text-xl mt-2">Liquidity Pool Uniswap</h3>
-          <CheckCircleIcon className="h-8 w-8 mt-2" />
-          <p>Locked for 1 Year</p>
-          <div>
+          <h3 className="text-xl text-center font-bold mb-2">Liquidity Pool Uniswap</h3>
+          <div className="flex justify-center mb-4">
+            <CheckCircleIcon className="h-8 w-8 text-green-400" />
+          </div>
+          <p className="mb-4 text-center font-semibold">Locked for 1 Year</p>
+          <p className="text-sm leading-relaxed">
             A substantial majority of our tokens are dedicated to ensuring robust liquidity on Uniswap. This lock-in
             period signifies our commitment to long-term stability and trust, preventing major fluctuations and
             providing a secure trading environment for all users.
-          </div>
+          </p>
         </div>
 
-        <div className="flex flex-col items-center">
-          <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center">
-            <span className="text-2xl">10%</span>
+
+        <div className="bg-gray-700 rounded-xl p-6 shadow-lg transition duration-300 ease-in-out hover:scale-105">
+          <div className="flex justify-center">
+            <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center mb-4">
+              <span className="text-3xl font-bold">10%</span>
+            </div>
           </div>
-          <h3 className="text-xl mt-2">Reserved Liquidity Pool </h3>
-          <CurrencyDollarIcon className="h-8 w-8 mt-2" />
-          <p>CEX</p>
-          <div>
+          <h3 className="text-xl text-center font-bold mb-2">Reserved Liquidity Pool</h3>
+          <div className="flex justify-center mb-4">
+            <CurrencyDollarIcon className="h-8 w-8 text-green-400" />
+          </div>
+          <p className="mb-4 text-center font-semibold">CEX</p>
+          <p className="text-sm leading-relaxed">
             To broaden accessibility and enhance trading options for our users, we've allocated a significant portion of
             our tokens for liquidity pools on centralized exchanges. This strategic reserve is essential for integrating
             into the wider crypto ecosystem, facilitating seamless transactions and enhancing user experience on various
             platforms.
-          </div>
+          </p>
         </div>
-        <div className="flex flex-col items-center">
-          <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center">
-            <span className="text-2xl">10%</span>
+
+        <div className="bg-gray-700 rounded-xl p-6 shadow-lg transition duration-300 ease-in-out hover:scale-105">
+          <div className="flex justify-center">
+            <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center mb-4">
+              <span className="text-3xl font-bold">10%</span>
+            </div>
           </div>
-          <h3 className="text-xl mt-2">Allocated for Staking </h3>
-          <CurrencyDollarIcon className="h-8 w-8 mt-2" />
-          <p>Rewards</p>
-          <div>
-            To enrich the Nomota AI experience and incentivize our community, we've allocated 10% of our tokens
+          <h3 className="text-xl text-center font-bold mb-2">Allocated for Staking</h3>
+          <div className="flex justify-center mb-4">
+            <CurrencyDollarIcon className="h-8 w-8 text-green-400" />
+          </div>
+          <p className="mb-4 text-center font-semibold">Rewards</p>
+          <p className="text-sm leading-relaxed">
+          To enrich the Nomota AI experience and incentivize our community, we've allocated 10% of our tokens
             specifically for staking rewards. This allocation is designed to reward users who invest in the stability
             and growth of our platform, offering a tangible appreciation for their confidence and support. By staking
             their tokens, users not only contribute to the security and robustness of Nomota AI but also earn rewards
             that can be used across our platform, including on bot usage and in our marketplace for VPS and GPU rentals.
-          </div>
+          </p>
         </div>
-        <div className="flex flex-col items-center">
-          <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center">
-            <span className="text-2xl">5%</span>
+
+        <div className="bg-gray-700 rounded-xl p-6 shadow-lg transition duration-300 ease-in-out hover:scale-105">
+          <div className="flex justify-center">
+            <div className="w-24 h-24 rounded-full border-4 border-red-500 flex items-center justify-center mb-4">
+              <span className="text-3xl font-bold">5%</span>
+            </div>
           </div>
-          <h3 className="text-xl mt-2">Allocated to Investors</h3>
-          <CurrencyDollarIcon className="h-8 w-8 mt-2" />
-          <p>Vesting Period of 6 Weeks</p>
-          <div>
-            Recognizing the crucial role of our early supporters, we've earmarked a portion of tokens for our investors.
-            This allocation comes with a vesting period of 6 weeks, ensuring our investors are aligned with the
-            long-term vision and success of Nomota AI, while also allowing for gradual market entry.
+          <h3 className="text-xl text-center font-bold mb-2">Allocated for Investors</h3>
+          <div className="flex justify-center mb-4">
+            <CurrencyDollarIcon className="h-8 w-8 text-green-400" />
           </div>
+          <p className="mb-4 text-center font-semibold">Vesting Period of 6 weeks</p>
+          <p className="text-sm leading-relaxed">
+            To enrich the Nomota AI experience and incentivize our community, we've allocated 10% of our tokens specifically for staking rewards. This allocation is designed to reward users who invest in the stability and growth of our platform, offering a tangible appreciation for their confidence and support. By staking
+            their tokens, users not only contribute to the security and robustness of Nomota AI but also earn rewards
+            that can be used across our platform, including on bot usage and in our marketplace for VPS and GPU rentals.
+          </p>
         </div>
+       
       </div>
     </div>
   );
