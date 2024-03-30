c8d35d3a9c51eaf687df3cbeb94caa738ba3d758
mohan varma <mohanvarma.k@gmail.com>
2024-03-30 14:45:15 -0600

    adding pretieer and also some road map changes

--- src/components/FeaturesSection/index.tsx
+++ src/components/FeaturesSection/index.tsx
@@ -1,49 +1,49 @@
 import React from 'react';
 import FeatureTile from './FeatureTile';
-import './FeatureTile.css'; 
+import './FeatureTile.css';
 
 const featuresData = [
   {
     title: 'VPS / GPU Marketplace',
     description: 'Rent out or hire VPS and GPU.',
-    imageUrl: 'image-generation.webp', 
-    linkUrl: '/products/marketplace', 
+    imageUrl: 'image-generation.webp',
+    linkUrl: '/products/marketplace',
   },
   {
     title: 'Real-Time Chat',
     description: 'Our bots offer real-time chat assistance.',
-    imageUrl: 'real-time-chat.png', 
+    imageUrl: 'real-time-chat.png',
     linkUrl: '/products/chatbot',
   },
   {
     title: 'Image Generation',
     description: 'Innovative image generation.',
-    imageUrl: 'image-generation.webp', 
-    linkUrl: '/products/image-generation', 
+    imageUrl: 'image-generation.webp',
+    linkUrl: '/products/image-generation',
   },
   {
     title: 'Voice Synthesis',
     description: 'Lifelike voice synthesis.',
-    imageUrl: 'real-time-chat.png', 
-    linkUrl: '/products/voice-synthesis', 
+    imageUrl: 'real-time-chat.png',
+    linkUrl: '/products/voice-synthesis',
   },
   {
     title: 'Music Generation',
     description: 'Magic music generation.',
-    imageUrl: 'image-generation.webp', 
-    linkUrl: '/products/music-generation', 
+    imageUrl: 'image-generation.webp',
+    linkUrl: '/products/music-generation',
   },
   {
     title: 'Staking',
     description: 'Stake your $NAI for 7% to 15% APY',
-    imageUrl: 'image-generation.webp', 
-    linkUrl: '/products/staking', 
+    imageUrl: 'image-generation.webp',
+    linkUrl: '/products/staking',
   },
 ];
 
 const FeaturesSection: React.FC = () => {
   return (
-    <div className="py-12 bg-gray-100 max-md:p-8">
+    <div className="py-12 bg-white max-md:p-8">
       <div className="mb-8 text-center">
         <h2 className="text-3xl font-bold">Our products</h2>
       </div>
--- src/components/Main/Roadmap/index.tsx
+++ src/components/Main/Roadmap/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import React from 'react';
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
 
 const Roadmap: React.FC = () => {
   return (
-    <div className="bg-gray-900 text-white py-12 px-4">
+    <div className="bg-white text-white py-12 px-4">
       <div className="text-center mb-8">
-        <h2 id="roadmap" className="text-3xl font-bold">Roadmap</h2>
+        <h2 id="roadmap" className="text-3xl font-bold text-black">
+          Roadmap
+        </h2>
         <div className="h-1 bg-red-600 w-full my-5" />
       </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap justify-center md:items-center gap-8">
+      <div className="flex flex-col md:flex-row md:flex-wrap justify-center md:items-center gap-8 max-w-6xl mx-auto">
         <div className="flex flex-col items-center text-sm bg-gray-700 p-4 rounded-md shadow-md space-x-2">
           <div className="flex items-center space-x-2">
             <CheckCircleIcon className="h-5 w-5 text-green-500" />
