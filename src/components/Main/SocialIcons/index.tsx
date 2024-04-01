ecea2eb3501e56b5d4bdc14b8205601033ad3555
casperdeleuw <casperdeleuw@gmail.com>
2024-04-01 20:43:33 +0200

    TG link

--- src/components/Main/SocialIcons/index.tsx
+++ src/components/Main/SocialIcons/index.tsx
@@ -15,7 +15,7 @@ const SocialIcons: React.FC = () => {
         </svg>
       </a>
 
-      {/* <a href="https://t.me/nomotaai_portal" className="transition-colors duration-200 hover:text-blue-500">
+      <a href="https://t.me/nomotaai" className="transition-colors duration-200 hover:text-blue-500">
         <svg
           aria-label="Telegram"
           className="w-6 h-6 fill-current"
@@ -25,7 +25,7 @@ const SocialIcons: React.FC = () => {
         >
           <path d="M9.036 15.464l-.932 3.285c-.096.337.418.478.622.2l1.27-1.74 2.457 1.827c.418.313.774.2.905-.295l3.38-15.112c.13-.496-.291-.713-.62-.498L.725 9.607c-.497.238-.498.498-.025.635l3.3 1.054 7.338-4.595c.335-.21.64-.04.283.04z" />
         </svg>
-      </a> */}
+      </a>
     </div>
   );
 };
